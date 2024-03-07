1e4cd427461b9a3312d0c1f5e5848a83095fed92
Maciej Krüger <mkrueger@mgit.at>
2024-03-07 17:18:53 +0100

    Add packages stub

--- flake.nix
+++ flake.nix
@@ -3,8 +3,24 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
-    nixosModules = with nixpkgs.lib;
+  outputs = { self, nixpkgs }: with nixpkgs.lib; {
+    overlays.default = import ./overlay.nix;
+
+    packages.x86_64-linux = let
+      pkgs = import "${nixpkgs}" {
+        system = "x86_64-linux";
+        overlays = [ self.overlays.default ];
+      };
+      folder = builtins.readDir ./pkgs;
+    in {
+      default = pkgs.releaseTools.aggregate {
+        name = "mgit-nixos-pkgs";
+
+        constituents = mapAttrsToList (pkg: _: pkgs.${pkgs}) folder;
+      };
+    } // (mapAttrs (pkg: _: pkgs.${pkg}) folder);
+
+    nixosModules =
       let
         modules = mapAttrs' (key: _:
           nameValuePair
---
+++ overlay.nix
@@ -0,0 +1,1 @@
+final: prev: prev.lib.mapAttrs (pkg: _: prev.callPackage "${./pkgs}/${pkg}" {}) (builtins.readDir ./pkgs)
