df046af00919f509ca720c808ee91419e9558b6e
Maciej Krüger <mkrueger@mgit.at>
2024-03-17 22:54:39 +0100

    ethtool-setringmax: use restarts

--- modules/ethtool-setringmax.nix
+++ modules/ethtool-setringmax.nix
@@ -9,12 +9,10 @@ with lib;
     systemd.services.ethtool-setringmax = {
       after = [ "network.target" ];#
       wantedBy = [ "multi-user.target" ];
-      reloadIfChanged = true;
       serviceConfig = {
         Description = "Set Ring Parameters for Network Interfaces";
         Type = "oneshot";
         ExecStart = "${pkgs.ethtool-setringmax}/bin/ethtool-setringmax";
-        ExecReload = "${pkgs.ethtool-setringmax}/bin/ethtool-setringmax";
         RemainAfterExit = true;
       };
     };
