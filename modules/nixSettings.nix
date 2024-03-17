bf260679aac2986a88bc33e2294df874b8398431
Maciej Krüger <mkrueger@mgit.at>
2024-03-17 23:17:20 +0100

    nixSettings: add gc

--- modules/nixSettings.nix
+++ modules/nixSettings.nix
@@ -1,8 +1,17 @@
+{ options, lib, ... }@args:
+import ./defaults/_with_unify.nix args true
 {
   nix.settings = {
     # Enable flakes and new 'nix' command
     experimental-features = "nix-command flakes";
     # Deduplicate and optimize nix store
     auto-optimise-store = true;
+  };
+  nix.gc.automatic = true;
+  nix.gc.options = "--delete-older-than 14d";
+}
+{
+  nixUnify = {
+    modules.shareSystemd.units = [ "nix-gc.timer" ];
   };
 }
