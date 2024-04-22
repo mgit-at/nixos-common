c9b137c2c671e2b64b80cea4697b093008e65f43
Maciej Krüger <mkrueger@mgit.at>
2024-04-22 15:43:04 +0200

    ansible-mgit: fixes for mgssl

--- pkgs/ansible-mgit/default.nix
+++ pkgs/ansible-mgit/default.nix
@@ -1,13 +1,23 @@
 { ansible
+, python3
 , python3Packages
 }:
 
-ansible.overrideAttrs (a: {
-  name = "${a.name}-MGIT";
-  # add missing hcloud dependency to ansible
-  propagatedBuildInputs = a.propagatedBuildInputs ++ (with python3Packages; [
+let
+  extraPy = ps: with ps; [
     hcloud
     cryptography
     pyopenssl
-  ]);
+  ];
+  py = python3.withPackages extraPy;
+in
+ansible.overrideAttrs (a: {
+  name = "${a.name}-MGIT";
+  # add missing hcloud dependency to ansible
+  propagatedBuildInputs = a.propagatedBuildInputs ++ (extraPy python3Packages);
+
+  preFixup = ''
+    makeWrapperArgs+=("''${gappsWrapperArgs[@]}")
+    makeWrapperArgs+=(--prefix PYTHONPATH : "${py}/${py.sitePackages}")
+  '';
 })
