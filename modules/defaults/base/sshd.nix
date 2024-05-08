d46372d382290f60d7d0d7cae191b7eaeb32620a
Maciej Krüger <mkrueger@mgit.at>
2024-05-08 22:29:37 +0200

    base: disable authorized keys in ~/.ssh/authorized_keys

--- modules/defaults/base/sshd.nix
+++ modules/defaults/base/sshd.nix
@@ -15,6 +15,9 @@ with lib;
       UsePAM = true;
     };
 
+    # disable ~/.ssh/authorized_keys (default in 24.11)
+    authorizedKeysInHomedir = false;
+
     # https://gitlab.com/gitlab-org/gitlab-foss/-/blob/master/doc/user/gitlab_com/index.md#ssh-host-keys-fingerprints
     knownHosts."gitlab.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
     # https://github.blog/2021-09-01-improving-git-protocol-security-github/#new-host-keys
