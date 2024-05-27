c110a178838ceea739e45a2f542ec247c79bb1f8
Maciej Krüger <mkrueger@mgit.at>
2024-05-27 15:39:29 +0200

    command-not-found: fix hack for newer nix

--- modules/nix-index-extra.nix
+++ modules/nix-index-extra.nix
@@ -1,7 +1,10 @@
+{ pkgs, ... }:
+
 let
+  empty = pkgs.writeTextDir "empty" "";
   createEmptyProfile = ''
     if [ ! -e "$HOME/.nix-profile/manifest.json" ]; then
-      nix profile upgrade >/dev/null 2>/dev/null || true
+      (nix profile install ${empty} && nix profile remove ${empty}) >/dev/null 2>/dev/null || true
     fi
   '';
 in {
