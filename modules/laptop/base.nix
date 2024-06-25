ebce6e5daa00e945e26c783a1fa5bbf2e444074d
Maciej Krüger <mkrueger@mgit.at>
2024-06-25 20:29:16 +0200

    laptop.base: add mgit.skipNixCache

--- modules/laptop/base.nix
+++ modules/laptop/base.nix
@@ -1,22 +1,39 @@
 { config, pkgs, lib, ... }: with lib; {
-  nix = {
-    settings = {
-      auto-optimise-store = true;
-      trusted-users = [ "root" "@wheel" ];
+  options = {
+    mgit = {
+      skipNixCache = mkEnableOption "skipping the nix-internal cache when checking the cache";
     };
-    package = pkgs.nixVersions.latest;
   };
 
-  programs.git = {
-    enable = true;
-    lfs.enable = true;
-  };
+  config = mkMerge [
+    {
+      nix = {
+        settings = {
+          auto-optimise-store = true;
+          trusted-users = [ "root" "@wheel" ];
+        };
+        package = pkgs.nixVersions.latest;
+      };
 
-  virtualisation.docker = {
-    enable = true;
-    # save resources
-    enableOnBoot = false;
-    # this causes problems on shutdown
-    liveRestore = false;
-  };
+      programs.git = {
+        enable = true;
+        lfs.enable = true;
+      };
+
+      virtualisation.docker = {
+        enable = true;
+        # save resources
+        enableOnBoot = false;
+        # this causes problems on shutdown
+        liveRestore = false;
+      };
+    }
+    (mkIf (config.mgit.skipNixCache) {
+      nix.settings = {
+        # Disable cache
+        narinfo-cache-positive-ttl = 0;
+        narinfo-cache-negative-ttl = 0;
+      };
+    })
+  ];
 }
