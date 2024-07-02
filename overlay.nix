077cb1dd47967721125ad5cd31f85e8696fb4c15
Maciej Krüger <mkrueger@mgit.at>
2024-07-02 16:44:32 +0200

    mkAnsibleDevShell: set LOCALE_ARCHIVE to avoid problems on non-nixos
    ref https://stackoverflow.com/questions/62287269/nix-shell-cannot-change-locale-warning

--- overlay.nix
+++ overlay.nix
@@ -1,6 +1,8 @@
 final: prev:
   (prev.lib.mapAttrs (pkg: _: prev.callPackage "${./pkgs}/${pkg}" {}) (builtins.readDir ./pkgs)) // {
     mkAnsibleDevShell = { extraAnsiblePy ? [], packages ? [], shellHook ? "", ... }@args: final.mkShell (args // {
+      LOCALE_ARCHIVE = "${final.glibcLocales}/lib/locale/locale-archive";
+
       packages = with final; packages ++ [
         (ansible-mgit.mkCustom extraAnsiblePy)
         age
