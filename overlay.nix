3cc7e34c5ecfa05bd2048b382bfb2e86bf77090e
Maciej Krüger <mkrueger@mgit.at>
2024-07-11 23:42:45 +0200

    Revert "prometheus-node-exporter: fix"
    This reverts commit f7326f3f5c5a01c96b683a195ad21894a90d681c.

--- overlay.nix
+++ overlay.nix
@@ -1,12 +1,5 @@
 final: prev:
-  let
-    args = {
-      prometheus-node-exporter = {
-        inherit (prev.darwin.apple_sdk.frameworks) CoreFoundation IOKit;
-      };
-    };
-  in
-  (prev.lib.mapAttrs (pkg: _: prev.callPackage "${./pkgs}/${pkg}" (if args ? pkg then args.${pkg} else {})) (builtins.readDir ./pkgs)) // {
+  (prev.lib.mapAttrs (pkg: _: prev.callPackage "${./pkgs}/${pkg}" {}) (builtins.readDir ./pkgs)) // {
     mkAnsibleDevShell = { extraAnsiblePy ? [], packages ? [], shellHook ? "", ... }@args: final.mkShell (args // {
       LOCALE_ARCHIVE = "${final.glibcLocales}/lib/locale/locale-archive";
 
