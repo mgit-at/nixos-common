72bce66aac7779548dd226b80b001824a797fd42
Maciej Krüger <mkrueger@mgit.at>
2024-08-23 22:39:36 +0200

    zsh: fix no matches found - see #4

--- modules/defaults/zsh/default.nix
+++ modules/defaults/zsh/default.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }: {
   programs.zsh = {
     enable = true;
-    interactiveShellInit = builtins.readFile ./zshrc;
+    interactiveShellInit = builtins.readFile ./zshrc + ''
+      # fixes "no matches found"
+      unsetopt EXTENDED_GLOB
+    '';
   };
 
   users.defaultUserShell = pkgs.zsh;
