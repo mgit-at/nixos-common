1cd1a113b5e688109d2a34d94b8891fcb87804de
Maciej Krüger <mkrueger@mgit.at>
2024-12-19 18:16:19 +0100

    ansible-mgit: expose v2_15 and v2_16

--- pkgs/ansible-mgit/default.nix
+++ pkgs/ansible-mgit/default.nix
@@ -1,5 +1,7 @@
-{ callPackage, ansible }:
+{ callPackage, ansible, ansible_2_16, ansible_2_15 }:
 
 (callPackage ./package.nix {}) // {
   mkCustom = { extraAnsiblePy, ansible ? ansible }: callPackage ./package.nix { inherit extraAnsiblePy ansible; };
+  v2_16 = callPackage ./package.nix { ansible = ansible_2_16; };
+  v2_15 = callPackage ./package.nix { ansible = ansible_2_15; };
 }
