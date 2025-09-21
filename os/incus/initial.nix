7f961629b8d59264ef5beca226873df88bccea25
Maciej Krüger <mkrueger@mgit.at>
2025-09-21 23:02:12 +0200

    fix: incus image

--- os/incus/initial.nix
+++ os/incus/initial.nix
@@ -1,7 +1,7 @@
 { inputs, lib, ... }: with lib; {
   imports = [
     # initial lxd
-    "${inputs.nixpkgs}/nixos/maintainers/scripts/lxd/lxd-container-image.nix"
+    "${inputs.nixpkgs}/nixos/maintainers/scripts/incus/incus-container-image.nix"
   ];
 
   # to ease deployment
