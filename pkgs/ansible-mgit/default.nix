668326503dfebf1905eaabe897a01bd9b056e9a6
Michael Gebetsroither <mgebetsroither@mgit.at>
2025-11-18 11:55:16 +0100

    add ansible v2.20

--- pkgs/ansible-mgit/default.nix
+++ pkgs/ansible-mgit/default.nix
@@ -1,7 +1,8 @@
-{ callPackage, ansible, ansible_2_19, ansible_2_18, ansible_2_17, ansible_2_16, ansible_2_15 }:
+{ callPackage, ansible, ansible_2_20, ansible_2_19, ansible_2_18, ansible_2_17, ansible_2_16, ansible_2_15 }:
 
 (callPackage ./package.nix {}) // {
   mkCustom = { extraAnsiblePy, ansible ? ansible }: callPackage ./package.nix { inherit extraAnsiblePy ansible; };
+  v2_20 = callPackage ./package.nix { ansible = ansible_2_20; };
   v2_19 = callPackage ./package.nix { ansible = ansible_2_19; };
   v2_18 = callPackage ./package.nix { ansible = ansible_2_18; };
   v2_17 = callPackage ./package.nix { ansible = ansible_2_17; };
